9129095deff6dcebb93999cc465c7165919a871b
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2025-05-15 21:51:56 +0530

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -442,10 +442,7 @@ data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn-ami-hvm-*-x86_64-gp2"]
-  }
+
 }
 
 data "aws_ami" "amazon_linux_23" {
